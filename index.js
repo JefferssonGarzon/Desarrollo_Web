d0b180cd1a05993245ad3e48a6414c76cee52480
Jeffersson_Garzon <jeffersson.garzon@utp.edu.co>
2021-03-04 00:34:01 -0500

    se cambio el puerto

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ const express = require("express");
 const app = express();
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/public"))
 
